a3eac21bdf40c6c7526dbc591e198ff00c2d7b43
Emmanuel Abban <emmanuel.abban251@gmail.com>
2022-08-20 23:28:13 -0400

    arraylist

---
+++ arraylist/ArrayListApplication.java
@@ -0,0 +1,69 @@
+package io.justice.datastructure.arraylist;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class ArrayListApplication {
+
+	public static void main(String[] args) {
+		List<Employee> employeeList = new ArrayList<>();
+		
+		employeeList.add(new Employee("Jane", "Jones", 123));
+		employeeList.add(new Employee("John", "Doe",4567));
+		employeeList.add(new Employee("Mary", "Smith", 22));
+		employeeList.add(new Employee("Mike", "Wilson", 3245));
+		//System.out.println(employeeList.indexOf(new Employee("John", "Doe",4567)));
+		employeeList.stream().forEach(System.out::println);
+		System.out.println("======");
+		employeeList.forEach(System.out::println);
+		System.out.println("======");
+
+		employeeList.stream().skip(1).limit(1).forEach(System.out::println);
+		System.out.println("======");
+
+		System.out.println(employeeList.get(1));
+
+		System.out.println("======");
+
+		System.out.println(employeeList.isEmpty());;
+		employeeList.set(1, new Employee("John", "Adams", 4568));
+		System.out.println("======");
+		employeeList.stream().forEach(System.out::println);
+		System.out.println("======");
+		employeeList.add(3, new Employee("John", "Doe",4567));
+		employeeList.stream().forEach(System.out::println);
+		System.out.println("======");
+		
+		Object[] employeeArrayObject = employeeList.toArray();
+		Stream.of(employeeArrayObject).forEach(System.out::println);
+		System.out.println("======array");
+		Employee[] employeeArray = employeeList.toArray(new Employee[employeeList.size()]);
+		Stream.of(employeeArray).forEach(System.out::println);
+		System.out.println("======array");
+		
+		System.out.println(employeeList.contains(employeeList.get(1)));
+		
+		System.out.println("======");
+		
+		System.out.println(employeeList.contains(new Employee("Mary", "Smith", 22)));
+		
+		System.out.println("======");
+		
+		System.out.println(employeeList.indexOf(new Employee("John", "Doe",4567)));
+		
+		System.out.println("======");
+		System.out.println(employeeList.remove(new Employee("Mary", "Smith", 22)));
+		System.out.println("======");
+		System.out.println(employeeList.remove(0));
+		System.out.println("======");
+		employeeList.stream().forEach(System.out::println);
+		System.out.println("======");
+		System.arraycopy(employeeArray, 1, employeeArray, 0, employeeArray.length-1);
+		Stream.of(employeeArray).forEach(System.out::println);
+
+
+
+	}
+
+}
